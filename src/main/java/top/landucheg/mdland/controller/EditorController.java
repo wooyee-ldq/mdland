9bbba10564c97362f9a7be30e524842dc22f2f71
wooyee-ldq <2632230944@qq.com>
2020-11-25 23:50:06 +0800

    编写service和router

--- src/main/java/top/landucheg/mdland/controller/EditorController.java
+++ src/main/java/top/landucheg/mdland/controller/EditorController.java
@@ -3,6 +3,7 @@ package top.landucheg.mdland.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import top.landucheg.mdland.service.HexoService;
 
 @Controller
@@ -11,13 +12,33 @@ public class EditorController {
     @Autowired
     HexoService hexoService;
 
+    /**
+     * 编辑器主页面
+     *
+     * @return html
+     */
     @GetMapping("")
     public String index(){
         return "static/html/mdEditor.html";
     }
 
-    @GetMapping("/release")
+    /**
+     * 创建新的文章
+     *
+     * @return json
+     */
+    @PostMapping("/newpost")
+    public String newPost(){
+        return hexoService.hexoNew();
+    }
+
+    /**
+     * 发布文章到博客
+     *
+     * @return json
+     */
+    @PostMapping("/release")
     public String release(){
-        return "static/html/mdEditor.html";
+        return hexoService.hexoRelease();
     }
 }
--- src/main/java/top/landucheg/mdland/service/HexoService.java
+++ src/main/java/top/landucheg/mdland/service/HexoService.java
@@ -12,15 +12,27 @@ public class HexoService {
     @Autowired
     EnvironmentUtil environmentUtil;
 
+    public String hexoNew(){
+        return this.hexoExec("hexo.new");
+    }
+
+    public String hexoRelease(){
+        StringBuilder strbd = new StringBuilder();
+        strbd.append(this.hexoClean()).append(System.lineSeparator()).append(this.hexoGenerate());
+        return strbd.toString();
+    }
+
     public String hexoClean(){
-        CommandExec cmd = CommandExec.getInstance();
-        cmd.setOrder(new CmdImpl(environmentUtil.getProperties("hexo.clean")));
-        return cmd.exec();
+        return this.hexoExec("hexo.clean");
     }
 
     public String hexoGenerate(){
+        return this.hexoExec("hexo.generate");
+    }
+
+    private String hexoExec(String orderKey){
         CommandExec cmd = CommandExec.getInstance();
-        cmd.setOrder(new CmdImpl(environmentUtil.getProperties("hexo.generate")));
+        cmd.setOrder(new CmdImpl(environmentUtil.getProperties(orderKey)));
         return cmd.exec();
     }
 }
